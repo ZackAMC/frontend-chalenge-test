2158fa3546afe6a5ba52fbe115108213f30fcf85
ZackAMC <andresmila01@gmail.com>
2024-09-20 15:57:47 -0500

    Eliminacion del componente RootLayout de page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import RootLayout from "./layout";
-
-export default function Home(): JSX.Element {
+export default function Home (): JSX.Element {
   return (
-    <RootLayout>
       <h1>Welcome to the Inlaze Next.js Starter!</h1>
-    </RootLayout>
   );
 }
