785d9316b08f8fb87a317d31422c693ece2bbb73
ZackAMC <andresmila01@gmail.com>
2024-11-18 21:45:24 -0500

    ajuste para reconocimiento de tipo d retorno

--- src/app/(overview)/genres/[name]/page.tsx
+++ src/app/(overview)/genres/[name]/page.tsx
@@ -1,4 +1,4 @@
-export default function Selectgenre({ params }: { params: { name: string } }){
+export default function Selectgenre({ params }: { params: { name: string } }): JSX.Element {
     return(
         <div>
             <h1>{params.name}</h1>
